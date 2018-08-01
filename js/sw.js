11cdbf4a3f83a86ad4ec2c21f5fb8f3fbeac8d9f
Isabela Popa <isabela.popa87@gmail.com>
2018-08-01 19:17:51 +0300

    Cache the URLs

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,39 @@
-self.addEventListener('fetch', function(event) {
+// Cache the URLs
+self.addEventListener('install', function (event) {
+    // add the urls to cache to an array
+    let cacheUrls = [
+        '/',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/index.html',
+        '/restaurant.html'
+    ];
+
+    event.waitUntil(
+        // Open a cache and cache the URLs
+        caches.open('restaurant-reviews-v1').then(function (cache) {
+            return cache.addAll(
+                cacheUrls
+            );
+        })
+    );
+});
+
+
+// Add listener for fetch event
+self.addEventListener('fetch', function (event) {
     console.log(event.request);
 });
